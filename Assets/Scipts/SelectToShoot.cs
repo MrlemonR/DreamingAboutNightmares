997cc290d7b4f63fe44284f031311d4e83b06d40
MrlemonR <reallimon46@gmail.com>
2025-12-14 21:11:25 +0300

    Update SelectToShoot.cs

--- Assets/Scipts/SelectToShoot.cs
+++ Assets/Scipts/SelectToShoot.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
 using TMPro;
+using Unity.VisualScripting;
 public class SelectToShoot : MonoBehaviour
 {
     public Camera cam;
@@ -11,9 +12,6 @@ public class SelectToShoot : MonoBehaviour
     public TMP_Text[] Texts; //1: Head, 2: Hands, 3: Legs
 
     bool textcourtinestart = false;
-    bool textColorcourtine = false;
-    bool RevoRotCourtine = false;
-    bool RevoMoveCourtine = false;
     void Start()
     {
         StartCoroutine(TextActive(0f, 0f));
@@ -33,47 +31,51 @@ public class SelectToShoot : MonoBehaviour
     }
     void mouseHover()
     {
-        Vector3 MousePos = Input.mousePosition;
-        MousePos.z = 10f;
-        MousePos = cam.ScreenToWorldPoint(MousePos);
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-
+        AimTarget newTarget = AimTarget.None;
         if (Physics.Raycast(ray, out hit, 10f))
         {
-            Debug.Log("Hit: " + hit.collider.gameObject.name);
             switch (hit.collider.gameObject.name)
             {
                 case "Headcollider":
-                    currentTarget = AimTarget.Head;
-                    if (!RevoRotCourtine) StartCoroutine(RevolverLookAt(0));
-                    if (!RevoMoveCourtine) StartCoroutine(RevolverMoveTo(0));
-                    if (!textColorcourtine) StartCoroutine(TextColorChange(0));
-                    RevoRotCourtine = true;
-                    RevoMoveCourtine = true;
+                    newTarget = AimTarget.Head;
                     break;
                 case "ArmCollider":
-                    currentTarget = AimTarget.Arm;
-                    if (!RevoRotCourtine) StartCoroutine(RevolverLookAt(1));
-                    if (!RevoMoveCourtine) StartCoroutine(RevolverMoveTo(1));
-                    if (!textColorcourtine) StartCoroutine(TextColorChange(1));
-                    RevoRotCourtine = true;
-                    RevoMoveCourtine = true;
+                    newTarget = AimTarget.Arm;
                     break;
                 case "LegCollider":
-                    currentTarget = AimTarget.Leg;
-                    if (!RevoRotCourtine) StartCoroutine(RevolverLookAt(2));
-                    if (!RevoMoveCourtine) StartCoroutine(RevolverMoveTo(2));
-                    if (!textColorcourtine) StartCoroutine(TextColorChange(2));
-                    RevoRotCourtine = true;
-                    RevoMoveCourtine = true;
+                    newTarget = AimTarget.Leg;
                     break;
             }
         }
+        if (newTarget != currentTarget)
+        {
+            currentTarget = newTarget;
+            StopAllCoroutines();
+            if (currentTarget != AimTarget.None)
+            {
+                int index = TargetToIndex(currentTarget);
+                StartCoroutine(RevolverLookAt(index));
+                StartCoroutine(RevolverMoveTo(index));
+                StartCoroutine(TextColorChange(index));
+            }
+        }
     }
+    int TargetToIndex(AimTarget target)
+    {
+        switch (target)
+        {
+            case AimTarget.Head: return 0;
+            case AimTarget.Arm: return 1;
+            case AimTarget.Leg: return 2;
+            default: return 0;
+        }
+    }
+
+
     IEnumerator RevolverLookAt(int index)
     {
-        RevoRotCourtine = true;
         Quaternion startRot = revolver.transform.localRotation;
         Quaternion endRot = LookPositions[index].localRotation;
         float duration = 0.5f;
@@ -86,12 +88,10 @@ public class SelectToShoot : MonoBehaviour
             yield return null;
         }
         revolver.transform.localRotation = endRot;
-        RevoRotCourtine = false;
     }
 
     IEnumerator RevolverMoveTo(int index)
     {
-        RevoMoveCourtine = true;
         Vector3 startPos = revolver.transform.localPosition;
         Vector3 endPos = MovePositions[index].localPosition;
         float duration = 0.5f; float t = 0f;
@@ -101,7 +101,6 @@ public class SelectToShoot : MonoBehaviour
             yield return null;
         }
         revolver.transform.localPosition = endPos;
-        RevoMoveCourtine = false;
     }
 
     IEnumerator TextActive(float targetAlpha, float targetSpeed)
@@ -133,7 +132,6 @@ public class SelectToShoot : MonoBehaviour
     }
     IEnumerator TextColorChange(int selectedText)
     {
-        textColorcourtine = true;
         float elapsed = 0f;
         Color[] startColors = new Color[Texts.Length];
         for (int i = 0; i < Texts.Length; i++)
@@ -156,7 +154,6 @@ public class SelectToShoot : MonoBehaviour
         {
             Texts[i].color = (i == selectedText) ? Color.red : Color.white;
         }
-        textColorcourtine = false;
     }
     void RevolverShake(float ShakeAmout)
     {
